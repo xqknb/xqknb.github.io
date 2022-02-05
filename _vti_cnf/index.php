70787d240f18a7b65952a2fff51ff19a6725e307
xqknb <xqknb@qq.com>
2022-02-05 22:34:06 +0800

    upload my web

---
+++ _vti_cnf/index.php
@@ -0,0 +1,20 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|19 Oct 2021 10:36:13 -0000
+vti_extenderversion:SR|12.0.0.6211
+vti_author:SR|YEYE的电脑\\23919
+vti_modifiedby:SR|YEYE的电脑\\23919
+vti_timecreated:TR|19 Oct 2021 10:36:13 -0000
+vti_cacheddtm:TX|19 Oct 2021 10:36:13 -0000
+vti_filesize:IR|1061
+vti_cachedtitle:SR|Test Page
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|H|mailto:dkcocto@gmail.com H|http://www.kslabs.ru/ H|index.php
+vti_cachedsvcrellinks:VX|NHUS|mailto:dkcocto@gmail.com NHHS|http://www.kslabs.ru/ FHUS|index.php
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
+vti_charset:SR|utf-8
+vti_title:SR|Test Page
+vti_backlinkinfo:VX|index.php
